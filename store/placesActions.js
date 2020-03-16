c2affdf7ae17fb7889fc52c4854e06de6a50a362
ismail <ismailelmaslouhy@gmail.com>
2020-03-16 21:01:44 +0100

    work with FileSystem

--- store/placesActions.js
+++ store/placesActions.js
@@ -1,8 +1,25 @@
+import * as FileSystem from "expo-file-system";
+
 export const ADD_PLACE = "ADD_PLACE";
 
 export const addPlace = (title, imageUri) => {
-  return {
-    type: ADD_PLACE,
-    placeData: { title: title, imageUri: imageUri }
+  return async dispatch => {
+    const fileName = imageUri.split("/").pop();
+    const newPath = FileSystem.documentDirectory + fileName;
+
+    try {
+      await FileSystem.moveAsync({
+        from: imageUri,
+        to: newPath
+      });
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+
+    dispatch({
+      type: ADD_PLACE,
+      placeData: { title: title, imageUri: imageUri }
+    });
   };
 };
